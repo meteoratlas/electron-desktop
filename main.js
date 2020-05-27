c01e7f5ff5a11318e130e7374b88c672c8223b0c
meteoratlas <kellyaccnts@gmail.com>
2020-05-26 22:04:56 -0600

    update readme

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     mainWindow.loadFile("./example/index.html");
-    //mainWindow.setMenu(null);
+    mainWindow.setMenu(null);
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
