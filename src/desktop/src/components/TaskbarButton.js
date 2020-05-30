d55ddf800aa83e80f68ca022554c031af44781d5
meteoratlas <kellyaccnts@gmail.com>
2020-05-29 22:23:49 -0600

    taskbar functions, window embedded content experiment

--- src/desktop/src/components/TaskbarButton.js
+++ src/desktop/src/components/TaskbarButton.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 // represents a minimized window
-const TaskbarButton = () => {
-    return <div></div>;
+const TaskbarButton = (props) => {
+    // whether this button represents the currently operated window
+    const [selected, setSelected] = useState(false);
+    return (
+        <div className="taskbar-button" onClick={props.minOrMaximize}>
+            <p>{props.title}</p>
+        </div>
+    );
 };
 
 export default TaskbarButton;
--- src/desktop/src/components/Window.js
+++ src/desktop/src/components/Window.js
@@ -15,7 +15,17 @@ const Window = (props) => {
                     onMaximize={onMaximizeClicked}
                     onClose={onCloseClicked}
                 />
-                Window Content
+                {/* TODO: resizing the window containing the iframe is weirdly unresponse... investigate further.
+                Seems to only happen when making the window smaller?
+                Will probably need to do this in js */}
+                <iframe
+                    src="https://weather.gc.ca/forecast/canada/index_e.html?id=ab"
+                    title="Colossal"
+                ></iframe>
+                {/* <iframe
+                    src="https://www.thisiscolossal.com/"
+                    title="Colossal"
+                ></iframe> */}
             </div>
         </Draggable>
     );
